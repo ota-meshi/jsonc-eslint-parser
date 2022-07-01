62b774d7b9aeb026e225e37a27d14ec5ade844fb
Yosuke Ota <otameshiyo23@gmail.com>
2022-07-01 21:19:32 +0000

    format

--- explorer/.eslintrc.js
+++ explorer/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    parserOptions: {
-        sourceType: "module",
-        ecmaVersion: 2020,
-    },
-    extends: [
-        "plugin:@ota-meshi/+vue3",
-        "plugin:@ota-meshi/+json",
-        "plugin:@ota-meshi/+prettier",
-    ],
-    rules: {
-        "node/no-unsupported-features/es-syntax": "off",
-        "node/no-missing-import": "off",
-    },
-}
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  extends: [
+    "plugin:@ota-meshi/+vue3",
+    "plugin:@ota-meshi/+json",
+    "plugin:@ota-meshi/+prettier",
+  ],
+  rules: {
+    "node/no-unsupported-features/es-syntax": "off",
+    "node/no-missing-import": "off",
+  },
+};
