a24d4e7171254ebe13c358afb2e75bd9b2cafbe1
James <5511220+Zamiell@users.noreply.github.com>
2025-12-03 23:49:56 -0500

    fix: eslint (#248)
    * fix: eslint
    
    * fix gitignore

--- explorer/shim/path.js
+++ explorer/shim/path.js
@@ -4,6 +4,7 @@ const path = {
 module.exports = new Proxy(path, {
   get(_t, p) {
     if (!path[p]) {
+      // eslint-disable-next-line no-console -- Log the missing path methods for debugging purposes.
       console.log(p);
     }
     return path[p];
