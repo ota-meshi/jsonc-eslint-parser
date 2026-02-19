875c732a9f0e3d9577308fa4f57f6becc30e6f33
Yosuke Ota <otameshiyo23@gmail.com>
2026-02-19 14:12:40 +0900

    chore: migrate from Vue CLI to Vite in explorer (#272)

--- explorer/shim/path.js
+++ explorer/shim/path.js
@@ -1,12 +1,15 @@
-const path = {
-  sep: "/",
-};
-module.exports = new Proxy(path, {
-  get(_t, p) {
-    if (!path[p]) {
-      // eslint-disable-next-line no-console -- Log the missing path methods for debugging purposes.
-      console.log(p);
-    }
-    return path[p];
-  },
-});
+export const sep = "/";
+
+export default new Proxy(
+    { sep },
+    {
+        get(_t, p) {
+            if (p === "default") return _t;
+            if (!_t[p]) {
+                // eslint-disable-next-line no-console -- Log the missing path methods for debugging purposes.
+                console.log(p);
+            }
+            return _t[p];
+        },
+    },
+);
